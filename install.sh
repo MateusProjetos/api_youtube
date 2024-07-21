d2cfd8ef78bb3f3c1cfe20f69656dc4f9e841037
MateusProjetos <157444313+MateusProjetos@users.noreply.github.com>
2024-07-20 22:09:25 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,8 +4,11 @@
 sudo apt update
 sudo apt upgrade -y
 
-# Instala o Python, pip e o pacote para ambientes virtuais
+# Instala o Python, pip, e pacotes para ambientes virtuais
 sudo apt install -y python3.8 python3-pip python3-venv git
+
+# Instala o Nginx
+sudo apt install -y nginx
 
 # Clona o repositório
 git clone https://github.com/MateusProjetos/api_youtube.git
@@ -20,32 +23,35 @@ source venv/bin/activate
 # Instala as dependências
 pip install -r requirements.txt
 
-# Instala o Nginx e o Certbot
-sudo apt install -y nginx certbot python3-certbot-nginx
+# Configura o Gunicorn como um serviço systemd
+sudo tee /etc/systemd/system/gunicorn.service <<EOF
+[Unit]
+Description=Gunicorn instance to serve api_youtube
+After=network.target
+
+[Service]
+User=root
+Group=www-data
+WorkingDirectory=/home/root/api_youtube
+Environment="PATH=/home/root/api_youtube/venv/bin"
+ExecStart=/home/root/api_youtube/venv/bin/gunicorn --workers 3 --bind 127.0.0.1:8000 main:app
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Inicia e habilita o serviço Gunicorn
+sudo systemctl start gunicorn
+sudo systemctl enable gunicorn
 
 # Remove a configuração padrão do Nginx
 sudo rm /etc/nginx/sites-enabled/default
-
-# Obtém e instala o certificado SSL com o Certbot
-sudo certbot certonly --nginx -d api-youtube.dcodeclub.top -d www.api-youtube.dcodeclub.top --non-interactive --agree-tos -m your-email@example.com
 
 # Cria o arquivo de configuração do Nginx
 sudo tee /etc/nginx/sites-available/api_youtube <<EOF
 server {
     listen 80;
     server_name api-youtube.dcodeclub.top www.api-youtube.dcodeclub.top;
-
-    # Redireciona todo o tráfego HTTP para HTTPS
-    return 301 https://\$host\$request_uri;
-}
-
-server {
-    listen 443 ssl;
-    server_name api-youtube.dcodeclub.top www.api-youtube.dcodeclub.top;
-
-    # Caminhos dos certificados SSL (já obtidos pelo Certbot)
-    ssl_certificate /etc/letsencrypt/live/api-youtube.dcodeclub.top/fullchain.pem;
-    ssl_certificate_key /etc/letsencrypt/live/api-youtube.dcodeclub.top/privkey.pem;
 
     location / {
         proxy_pass http://127.0.0.1:8000;
@@ -63,24 +69,3 @@ sudo nginx -t
 
 # Reinicia o Nginx
 sudo systemctl restart nginx
-
-# Cria o arquivo de configuração do Gunicorn
-sudo tee /etc/systemd/system/gunicorn.service <<EOF
-[Unit]
-Description=Gunicorn instance to serve api_youtube
-After=network.target
-
-[Service]
-User=root
-Group=www-data
-WorkingDirectory=/home/root/api_youtube
-Environment="PATH=/home/root/api_youtube/venv/bin"
-ExecStart=/home/root/api_youtube/venv/bin/gunicorn --workers 3 --bind 127.0.0.1:8000 main:app
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
-# Inicia e habilita o serviço do Gunicorn
-sudo systemctl start gunicorn
-sudo systemctl enable gunicorn
